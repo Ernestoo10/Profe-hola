7c5701ca141d0cda54f07c7191113aded3d83f40
Ernestoo10 <superiomg4567@gmail.com>
2025-11-25 21:08:06 -0300

    Add files via upload

---
+++ Inicio Sesión1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbBienvenido = new System.Windows.Forms.Label();
+            this.gpLogin = new System.Windows.Forms.GroupBox();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.lbContraseña = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.bttIniciar = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bttsalir = new System.Windows.Forms.Button();
+            this.gpLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbBienvenido
+            // 
+            this.lbBienvenido.AutoSize = true;
+            this.lbBienvenido.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBienvenido.Location = new System.Drawing.Point(332, 9);
+            this.lbBienvenido.Name = "lbBienvenido";
+            this.lbBienvenido.Size = new System.Drawing.Size(138, 25);
+            this.lbBienvenido.TabIndex = 0;
+            this.lbBienvenido.Text = "Bienvenid@";
+            // 
+            // gpLogin
+            // 
+            this.gpLogin.Controls.Add(this.bttsalir);
+            this.gpLogin.Controls.Add(this.pictureBox1);
+            this.gpLogin.Controls.Add(this.linkLabel3);
+            this.gpLogin.Controls.Add(this.linkLabel2);
+            this.gpLogin.Controls.Add(this.linkLabel1);
+            this.gpLogin.Controls.Add(this.bttIniciar);
+            this.gpLogin.Controls.Add(this.txtContraseña);
+            this.gpLogin.Controls.Add(this.txtUsuario);
+            this.gpLogin.Controls.Add(this.lbContraseña);
+            this.gpLogin.Controls.Add(this.lbUsuario);
+            this.gpLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpLogin.Location = new System.Drawing.Point(12, 37);
+            this.gpLogin.Name = "gpLogin";
+            this.gpLogin.Size = new System.Drawing.Size(767, 372);
+            this.gpLogin.TabIndex = 1;
+            this.gpLogin.TabStop = false;
+            this.gpLogin.Text = "Login AIEP";
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsuario.Location = new System.Drawing.Point(69, 116);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(80, 20);
+            this.lbUsuario.TabIndex = 2;
+            this.lbUsuario.Text = "Usuarios";
+            // 
+            // lbContraseña
+            // 
+            this.lbContraseña.AutoSize = true;
+            this.lbContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbContraseña.Location = new System.Drawing.Point(69, 180);
+            this.lbContraseña.Name = "lbContraseña";
+            this.lbContraseña.Size = new System.Drawing.Size(102, 20);
+            this.lbContraseña.TabIndex = 3;
+            this.lbContraseña.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(215, 116);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(359, 24);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(215, 180);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(359, 24);
+            this.txtContraseña.TabIndex = 5;
+            // 
+            // bttIniciar
+            // 
+            this.bttIniciar.Location = new System.Drawing.Point(288, 291);
+            this.bttIniciar.Name = "bttIniciar";
+            this.bttIniciar.Size = new System.Drawing.Size(153, 31);
+            this.bttIniciar.TabIndex = 6;
+            this.bttIniciar.Text = "Iniciar Sesión";
+            this.bttIniciar.UseVisualStyleBackColor = true;
+            this.bttIniciar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(127, 241);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(225, 15);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "¿Se te ha olvidado la contraseña?";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel2.Location = new System.Drawing.Point(393, 250);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(0, 15);
+            this.linkLabel2.TabIndex = 8;
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel3.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel3.Location = new System.Drawing.Point(393, 241);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(181, 15);
+            this.linkLabel3.TabIndex = 9;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "¿No te has registrado aún?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources._5095023;
+            this.pictureBox1.Location = new System.Drawing.Point(318, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bttsalir
+            // 
+            this.bttsalir.Location = new System.Drawing.Point(679, 340);
+            this.bttsalir.Name = "bttsalir";
+            this.bttsalir.Size = new System.Drawing.Size(82, 26);
+            this.bttsalir.TabIndex = 11;
+            this.bttsalir.Text = "Salir";
+            this.bttsalir.UseVisualStyleBackColor = true;
+            this.bttsalir.Click += new System.EventHandler(this.bttsalir_Click);
+            // 
+            // FrInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(791, 431);
+            this.Controls.Add(this.gpLogin);
+            this.Controls.Add(this.lbBienvenido);
+            this.Name = "FrInicio";
+            this.Text = "Inicio Sesión";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gpLogin.ResumeLayout(false);
+            this.gpLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbBienvenido;
+        private System.Windows.Forms.GroupBox gpLogin;
+        private System.Windows.Forms.Label lbUsuario;
+        private System.Windows.Forms.Label lbContraseña;
+        private System.Windows.Forms.Button bttIniciar;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bttsalir;
+    }
+}
+
---
+++ MENU.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MENU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbMenu = new System.Windows.Forms.GroupBox();
+            this.bttIngresar = new System.Windows.Forms.Button();
+            this.bttsalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gbMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbMenu
+            // 
+            this.gbMenu.Controls.Add(this.bttsalir);
+            this.gbMenu.Controls.Add(this.bttIngresar);
+            this.gbMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbMenu.Location = new System.Drawing.Point(383, 12);
+            this.gbMenu.Name = "gbMenu";
+            this.gbMenu.Size = new System.Drawing.Size(382, 416);
+            this.gbMenu.TabIndex = 0;
+            this.gbMenu.TabStop = false;
+            this.gbMenu.Text = "Menu AIEP";
+            // 
+            // bttIngresar
+            // 
+            this.bttIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttIngresar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bttIngresar.Location = new System.Drawing.Point(26, 96);
+            this.bttIngresar.Name = "bttIngresar";
+            this.bttIngresar.Size = new System.Drawing.Size(324, 70);
+            this.bttIngresar.TabIndex = 0;
+            this.bttIngresar.Text = "Ingresar";
+            this.bttIngresar.UseVisualStyleBackColor = true;
+            // 
+            // bttsalir
+            // 
+            this.bttsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttsalir.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bttsalir.Location = new System.Drawing.Point(26, 254);
+            this.bttsalir.Name = "bttsalir";
+            this.bttsalir.Size = new System.Drawing.Size(324, 70);
+            this.bttsalir.TabIndex = 2;
+            this.bttsalir.Text = "Salir";
+            this.bttsalir.UseVisualStyleBackColor = true;
+            this.bttsalir.Click += new System.EventHandler(this.bttsalir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.AIEP_alt_azul_1;
+            this.pictureBox1.Location = new System.Drawing.Point(41, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MENU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gbMenu);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "MENU";
+            this.Text = "MENU";
+            this.Load += new System.EventHandler(this.MENU_Load);
+            this.gbMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbMenu;
+        private System.Windows.Forms.Button bttsalir;
+        private System.Windows.Forms.Button bttIngresar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
